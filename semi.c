94f24d74964d6a1c31ee762f932b436dcc6c7727
cs25-7z <s14525@salesio-sp.ac.jp>
2017-05-25 11:41:40 +0900

    forのなかみをつくりました．
    Signed-off-by: cs25-7z <s14525@salesio-sp.ac.jp>

--- semi.c
+++ semi.c
@@ -3,8 +3,13 @@
 void main(){
     int i;    
     char deadline=7;
-    for(i=0;i<deadline;i++){
-        
+    for(i=deadline;i>=0;i--){
+        if(i==0){
+            printf(".\n.\n.\nThe End");
+        }else{
+            printf("remaining - %d Days",i);
+            getchar();
+        }
     }
+    getchar();
 }
-        
